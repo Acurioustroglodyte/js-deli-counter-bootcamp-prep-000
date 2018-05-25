6f54f99b2ea4d4896816050ee33d48e37baeaf7f
Trevor Jameson <tjameson15@gmail.com>
2018-05-25 20:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
+function nowServing(line) {
+  if (line === []) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return line.pop();
+  }
+}
