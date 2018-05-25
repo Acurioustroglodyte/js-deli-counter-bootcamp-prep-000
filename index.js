212bf2128773181fa54327d92cbd0290163d33ca
Trevor Jameson <tjameson15@gmail.com>
2018-05-25 20:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    line.shift();
+    `Currently serving ${line.shift()}`;
   }
 }
 
