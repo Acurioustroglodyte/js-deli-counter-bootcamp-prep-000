efeb9e0835bb07dcc83d9265bc22f0f142ebbf9f
Trevor Jameson <tjameson15@gmail.com>
2018-05-25 20:27:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
-  var lineOrder = "The line is currently ";
+  var lineOrder = "The line is currently: ";
   if (line.length === 0) {
     return "The line is currently empty.";
   } else {
     for (var i = 0; i < line.length; i++) {
-      lineOrder += `${i + 1}\. ${line[i]},`;
+      lineOrder += `${i + 1}\. ${line[i]}, `;
     }
     return lineOrder;
   }
