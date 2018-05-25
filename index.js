c9f1adc786ee70ff20bf1a12346c69fa506c5910
Trevor Jameson <tjameson15@gmail.com>
2018-05-25 20:22:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
+function 
+
 function currentLine(line) {
   var lineOrder = "The line is currently ";
   if (line.length === 0) {
     return "The line is currently empty.";
   } else {
     for (var i = 0; i < line.length; i++) {
-      lineOrder += `${i}\. ${line[i]},`;
+      lineOrder += `${i}. ${line[i]},`;
     }
     return lineOrder;
   }
