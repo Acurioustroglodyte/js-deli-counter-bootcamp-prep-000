41972d9047711f5deb70e380e1feaab3d7073b13
Trevor Jameson <tjameson15@gmail.com>
2018-05-25 19:51:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function takeANumer(line, person) {
+  line = [...line, person];
+  var personIndex = line.findIndex(person);
+  return `Welcome, ${person}. You are number ${personIndex} in line.`;
+}
+
+function nowServing() {
+  if ()
+}
+
+function currentLine(line) {
+  if (line === []) {
+    return "The line is currently empty.";
+  } else {
+    return 
+  }
+}
